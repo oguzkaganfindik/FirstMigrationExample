f893aed9739177a2387437fe23a5e32abaa0f114
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-08-09 02:55:49 +0300

    Add project files.

---
+++ Context/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationOrnegi.Models;
+
+namespace MigrationOrnegi.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Urun> Urun { get; set; }
+    }
+}
---
+++ Models/Urun.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MigrationOrnegi.Models
+{
+    public class Urun
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage="{0} girmek zorundasınız")]
+        [Display(Name ="Ürün Kodu")]
+        [StringLength(50,ErrorMessage ="{0} maksimum {1} karakterden uzun olamaz.")]
+        public string UrunKodu { get; set; }
+
+        [Required(ErrorMessage = "{0} girmek zorundasınız")]
+        [Display(Name = "Ürün Tanım")]
+        [StringLength(100, ErrorMessage = "{0} maksimum {1} karakterden uzun olamaz.")]
+        public string UrunTanim { get; set; }
+
+        [Required(ErrorMessage = "{0} girmek zorundasınız")]
+        public int Miktar { get; set; }
+
+        public bool Aktif { get; set; }
+        public string AktifString => Aktif ? "Aktif" : "Pasif";
+    }
+}
